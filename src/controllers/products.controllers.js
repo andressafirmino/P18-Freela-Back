cf8aa02941e947fcff6323530051fae356352adb
andressafirmino <andressafirmino78@gmail.com>
2023-08-12 17:06:14 -0300

    feat: getProductsUser

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -1,4 +1,5 @@
-import { checkEmail } from "../repository/users.repository";
+import { getAllProducts, getUserProducts, postProduct } from "../repository/products.repository";
+import { checkEmail, checkToken } from "../repository/users.repository";
 
 export async function postProducts(req, res) {
     const { authorization } = req.headers;
@@ -14,7 +15,7 @@ export async function postProducts(req, res) {
         if (user.rows.length === 0) {
             return res.status(401).send({ message: "Usuário não autorizado!" });
         }
-        await postProducts(name, category, description, photos, idSeller, idStatus);
+        await postProduct(name, category, description, photos, idSeller, idStatus);
         res.statusSend(201);
     } catch (e) {
         res.status(500).send(e.message);
@@ -23,8 +24,28 @@ export async function postProducts(req, res) {
 
 export async function getProducts(req, res) {
     try {
-        const products = await getProducts();
+        const products = await getAllProducts();
         res.send(products.rows);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+}
+
+export async function getProductsUser(req, res) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+    const {id} = req.params;
+    try {
+        const logged = await checkToken(token);
+        if (logged.rows.length === 0) {
+            return res.status(401).send({ message: "Usuário não autorizado!" });
+        }
+        const user = await checkEmail(logged.rows[0].email);
+        if (user.rows.length === 0) {
+            return res.status(401).send({ message: "Usuário não autorizado!" });
+        }
+        const productsUser = await getUserProducts(user.rows[0].id);
+        res.send(productsUser);
     } catch (e) {
         res.status(500).send(e.message);
     }
--- src/repository/products.repository.js
+++ src/repository/products.repository.js
@@ -1,11 +1,16 @@
 import { db } from "../database/database.js";
 
-export function postProducts(name, category, description, photos, idSeller, idStatus) {
+export function postProduct(name, category, description, photos, idSeller, idStatus) {
     const res = db.query(`INSERT INTO products (name, category, description, photos, idSeller, idStatus) VALUES ($1, $2, $3, $4, $5, $6);`, [name, category, description, photos, idSeller, idStatus]);
     return res;
 }
 
-export function getProducts() {
+export function getAllProducts() {
     const res = db.query(`SELECT * FROM products`);
+    return res;
+}
+
+export function getUserProducts(id) {
+    const res = db.query(`SELECT * FROM products WHERE "idSeller" = $1;`, [id]);
     return res;
 }
